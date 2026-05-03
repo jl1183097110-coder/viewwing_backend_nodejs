312b0475c31015ac9bfcffba0eae0af09f6d22db
feng <jl1183097110@gmail.com>
2026-05-03 19:39:07 +0800

    update the limiter

--- src/middlewares/limiter.ts
+++ src/middlewares/limiter.ts
@@ -4,8 +4,8 @@ import { rateLimit } from 'express-rate-limit'
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: isProduction ? 100 : 10000000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	limit: isProduction ? 200 : 10000000, // Limit each IP to 200 requests per `window` (here, per 1 minutes).
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
